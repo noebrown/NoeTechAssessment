57a3e5185d103a5e1ac6942e5b4ebd86e98b4765
Noe Brown <noejbrown@gmail.com>
2026-01-21 12:26:37 -0500

    Clean up Code: - Added complexParse() logic to parse() method and removed complexParse() method - Removed rowScan() and rowScanPathList() methods - Removed reference of the above methods in remaining methods (verifyPathList(), parsePathList(),and parse())

--- src/Main.java
+++ src/Main.java
@@ -2,7 +2,7 @@ import java.io.BufferedReader;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.Objects;
+
 import java.util.Scanner;
 import java.util.ArrayList;
 import java.util.HashMap;
@@ -16,7 +16,6 @@ public class Main {
         Scanner getPath = new Scanner(System.in);
         System.out.printf("Please type the path of the CSV file to be parsed.\nIf you are parsing multiple CSV files please separate each path with a comma.\nDo not put any spaces in between comma and path.\nOnce finished press enter/return.%n");
         path = getPath.nextLine();
-        // System.out.println("Path(s):" + path);
 
         // Test CSV Files
         // /Users/noebrown/Desktop/athletes.csv
@@ -26,11 +25,7 @@ public class Main {
         // /Users/noebrown/Desktop/athletes.csv,/Users/noebrown/Desktop/ethPriceData.csv
         // /Users/noebrown/Desktop/athletes.csv,/Users/noebrown/Desktop/cars.csv,/Users/noebrown/Desktop/ethPriceData.csv,/Users/noebrown/Desktop/carsUnique.csv,
         verifyPathList();
-        rowScanPathList();
         parsePathList();
-
-
-
     }
 
     // Traverse the list of CSV paths and ensure each patch can be opened and closed
@@ -70,10 +65,9 @@ public class Main {
     public static void parsePathList() {
         String[] pathList = path.split(",");
         for (String s : pathList) parse(s);
-        for (String s : pathList) complexParse(s);
     }
 
-    public static ArrayList<HashMap<String, String>> complexParse(String CSVFile){
+    public static ArrayList<HashMap<String, String>> parse(String CSVFile) {
         String line = "";
         String[] columnHeaders = null;
         int numOfColumns = 0;
@@ -168,91 +162,4 @@ public class Main {
 
         return rows;
     }
-
-    // simplest parsing logic for "clean" CSV files
-    public static ArrayList<HashMap<String, String>> parse(String CSVFile) {
-        String line;
-        String[] columnHeaders = null;
-        int numOfColumns = 0;
-        ArrayList<HashMap<String, String>> rows = new ArrayList<>();
-
-        try {
-            BufferedReader bufferedReader = new BufferedReader(new FileReader(CSVFile));
-
-            // Read first line of CSV (ColumnHeaders) and save as keys
-            if ((line = bufferedReader.readLine()) != null) {
-                columnHeaders = line.split(",");
-                numOfColumns = columnHeaders.length;
-            }
-
-            // read rest of lines
-            while ((line = bufferedReader.readLine()) != null) {
-                String[] values = line.split(",");
-
-                HashMap<String, String> row = new HashMap<>();
-                for (int i = 0; i < Objects.requireNonNull(columnHeaders).length; i++) {
-                    row.put(columnHeaders[i], values[i]);
-                }
-
-                rows.add(row);
-            }
-
-            bufferedReader.close();
-
-            System.out.println(numOfColumns + " Columns");
-
-            // print rows
-            for (HashMap<String, String> rowMap : rows) {
-                System.out.println(rowMap);
-            }
-
-        // have to put these or error
-        } catch (FileNotFoundException e) {
-            System.err.println("File not found: " + CSVFile);
-            e.printStackTrace();
-        } catch (IOException e) {
-            System.err.println("Error reading file: " + CSVFile);
-            e.printStackTrace();
-        }
-
-        return rows;
-    }
-
-    public static void rowScanPathList() {
-        String[] pathList = path.split(",");
-        for (String s : pathList) System.out.println(rowScan(s));
-    }
-
-    public static boolean rowScan(String CSVFile) {
-        String line = "";
-        String[] columnHeaders = null;
-        int numOfCommaHeader = 0;
-        int numOfComma = 0;
-
-            try {
-                BufferedReader bufferedReader = new BufferedReader(new FileReader(CSVFile));
-
-                // Read first line of CSV (ColumnHeaders) and save as keys
-                if ((line = bufferedReader.readLine()) != null) {
-                    columnHeaders = line.split(",");
-                    numOfCommaHeader = columnHeaders.length - 1;
-                }
-
-                // read rest of line
-                for (int i=0; i<5 && ((line = bufferedReader.readLine()) != null); i++) {
-                        numOfComma = line.length() - line.replace(",", "").length();
-                }
-                bufferedReader.close();
-
-                // have to put these or error
-            } catch (FileNotFoundException e) {
-                System.err.println("File not found: " + CSVFile);
-                e.printStackTrace();
-            } catch (IOException e) {
-                System.err.println("Error reading file: " + CSVFile);
-                e.printStackTrace();
-            }
-            System.out.println("The number of Commas: " + numOfComma);
-        return numOfCommaHeader == numOfComma;
-        }
-    }
+}
